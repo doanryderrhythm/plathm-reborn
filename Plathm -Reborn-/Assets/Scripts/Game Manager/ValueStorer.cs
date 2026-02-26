3f5c9a8a695ba017f74e8452d013ead61be69ac0
Ryder <doanryder2005@gmail.com>
2026-02-26 11:30:12 +0700

    Made the player, player movement, coyote time, jump buffer time, and collision check

---
+++ Plathm -Reborn-/Assets/Scripts/Game Manager/ValueStorer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ValueStorer : MonoBehaviour
+{
+    #region PLAYER
+    public static float moveSpeed = 10f;
+    public static float jumpHeight = 27f;
+    public static float lightPush = 10f;
+
+    public static int maxJumpCount = 2;
+
+    public static float sizeRadiusStill = 0f;
+    public static float sizeRadiusMoving = 0.2f;
+
+    public static Vector2 colliderSizeStill = new Vector2(1f, 1f);
+    public static Vector2 colliderSizeMoving = new Vector2(0.5f, 0.5f);
+    #endregion
+
+    #region LAYER MASKS
+    public const string groundLM = "Ground";
+    #endregion
+
+    #region VALUES
+    public static float coyoteTime = 0.06f;
+    public static float bufferTime = 0.12f;
+    #endregion
+}
---
+++ Plathm -Reborn-/Assets/Scripts/Platformer Player/PlatformerPlayer.cs
@@ -0,0 +1,156 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlatformerPlayer : MonoBehaviour
+{
+    [SerializeField] InputActionReference playerRunInput;
+    [SerializeField] InputActionReference playerJumpInput;
+
+    void OnEnable()
+    {
+        playerRunInput.action.Enable();
+        playerJumpInput.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerRunInput.action.Disable();
+        playerJumpInput.action.Disable();
+    }
+
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] BoxCollider2D bc;
+    private int jumpCount = 2;
+    private bool wasGrounded = false;
+    [SerializeField] TMP_Text jumpCountText;
+
+    //Landing Detection
+    [SerializeField] private float maxDistance = 0.1f;
+    [SerializeField] Collider2D boxCol;
+    private RaycastHit2D hit;
+    private bool hitDetected;
+    private bool leaveDetected;
+
+    //Buffer
+    private float jumpBufferTime;
+    private float coyoteTime;
+
+    [SerializeField] GameObject bufferJumpPrefab;
+
+    void ManageMove()
+    {
+        float moveRate = playerRunInput.action.ReadValue<float>();
+        rb.linearVelocityX = moveRate * ValueStorer.moveSpeed;
+
+        ChangeColliderShape(moveRate);
+    }
+
+    void ManageLeave()
+    {
+        if (!hitDetected && !leaveDetected)
+        {
+            coyoteTime -= Time.deltaTime;
+            if (coyoteTime <= 0)
+            {
+                jumpCount -= 1;
+                leaveDetected = true;
+            }
+        }
+    }
+
+    void ManageJump()
+    {
+        if (playerJumpInput.action.WasPressedThisFrame())
+        {
+            jumpBufferTime = ValueStorer.bufferTime;
+            Instantiate(bufferJumpPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            jumpBufferTime -= Time.deltaTime;
+        }
+
+        if (jumpBufferTime >= 0 && jumpCount > 0)
+        {
+            rb.linearVelocityY = 0;
+            rb.AddForceY(ValueStorer.jumpHeight, ForceMode2D.Impulse);
+            jumpCount -= 1;
+            leaveDetected = true;
+
+            jumpBufferTime = 0;
+        }
+        else if (playerJumpInput.action.WasReleasedThisFrame() && rb.linearVelocityY > 0)
+        {
+            rb.AddForceY(-ValueStorer.lightPush, ForceMode2D.Impulse);
+        }
+
+        jumpCountText.text = "Jump Count: " + jumpCount;
+    }
+
+    void ManageLand()
+    {
+        Vector2 halfExtents = boxCol.bounds.extents;
+        Vector2 origin = boxCol.bounds.center;
+        Vector2 direction = Vector2.down;
+
+        hit = Physics2D.BoxCast(origin, halfExtents, 0f, Vector2.down, maxDistance, LayerMask.GetMask(ValueStorer.groundLM));
+
+        hitDetected = hit.collider != null;
+
+        if (hitDetected && !wasGrounded)
+        {
+            coyoteTime = ValueStorer.coyoteTime;
+
+            leaveDetected = false;
+            jumpCount = ValueStorer.maxJumpCount;
+        }
+
+        wasGrounded = hitDetected;
+    }
+
+    void ChangeColliderShape(float moveRate)
+    {
+        if (moveRate != 0f)
+        {
+            bc.edgeRadius = ValueStorer.sizeRadiusMoving;
+            bc.size = ValueStorer.colliderSizeMoving;
+        }
+        else
+        {
+            bc.edgeRadius = ValueStorer.sizeRadiusStill;
+            bc.size = ValueStorer.colliderSizeStill;
+        }
+    }
+
+    void Awake()
+    {
+        jumpCount = ValueStorer.maxJumpCount;
+
+        jumpBufferTime = 0;
+        coyoteTime = ValueStorer.coyoteTime;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        ManageMove();
+        ManageJump();
+
+        if (Keyboard.current != null && Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        ManageLeave();
+        ManageLand();
+    }
+}
