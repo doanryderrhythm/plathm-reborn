9ea94c2e2a1c01afeae0dc62493eba5c4f5367a5
Ryder <doanryder2005@gmail.com>
2026-05-07 08:40:06 +0700

    Added the undo and redo shortcut

--- Plathm -Reborn-/Assets/Scripts/Undo & Redo/UndoRedoManager.cs
+++ Plathm -Reborn-/Assets/Scripts/Undo & Redo/UndoRedoManager.cs
@@ -1,16 +1,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class UndoRedoManager : MonoBehaviour
 {
     public Stack<EditorCommand> undoStack;
     public Stack<EditorCommand> redoStack;
 
-    private void Start()
+    [SerializeField] InputActionReference undoAction;
+    [SerializeField] InputActionReference redoAction;
+
+    private void Awake()
     {
         undoStack = new Stack<EditorCommand>();
         redoStack = new Stack<EditorCommand>();
+    }
+
+    private void OnEnable()
+    {
+        undoAction.action.performed += OnUndoPerformed;
+        undoAction.action.Enable();
+
+        redoAction.action.performed += OnRedoPerformed;
+        redoAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        undoAction.action.performed -= OnUndoPerformed;
+        undoAction.action.Disable();
+
+        redoAction.action.performed -= OnRedoPerformed;
+        redoAction.action.Disable();
     }
 
     public void ExecuteCommand(EditorCommand cmd)
@@ -32,6 +54,16 @@ public class UndoRedoManager : MonoBehaviour
         }
 
         redoStack.Clear();
+    }
+
+    public void OnUndoPerformed(InputAction.CallbackContext context)
+    {
+        ExecuteUndo();
+    }
+
+    public void OnRedoPerformed(InputAction.CallbackContext context)
+    {
+        ExecuteRedo();
     }
 
     public void ExecuteUndo()
